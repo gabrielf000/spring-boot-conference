9e02b277ce80daaa1ab98e8d85821545c1a6052a
Gabriel-Rosa <gfernandes.dsv@gmail.com>
2020-04-14 17:11:32 -0300

    Adjusting db credential to deploy

---
+++ src/src/main/java/com/gfr/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,21 @@
+package com.gfr.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+// Another way to configure the db connection
+@Configuration
+public class PersistenceConfiguration {
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("1234");
+//        System.out.println("My custom datasource bean has been initialized and set");
+//        return builder.build();
+//    }
+}
